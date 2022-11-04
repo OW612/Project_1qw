26f9d309720e48fe0616437b64067ebf11728d72
OW612 <116336696+OW612@users.noreply.github.com>
2022-11-04 17:50:37 +0300

    Add files via upload

---
+++ ЛР 1 1/ЛР 1 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ class Program
+ {
+   static void Main()
+   {
+      Console.Write("a=");
+      int a = int.Parse(Console.ReadLine());
+
+      Console.Write("b=");
+      int b = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("{0}+{1}={1}+{0}", a, b);
+
+   }
+ }
